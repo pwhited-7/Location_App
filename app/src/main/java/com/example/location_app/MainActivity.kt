e403cb707580b8c901c3cce16eb86d7e869b6d96
pwhited-7 <pacen-w77@hotmail.com>
2021-12-03 21:50:56 -0700

    Added google API

--- app/src/main/java/com/example/location_app/MainActivity.kt
+++ app/src/main/java/com/example/location_app/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.location_app
 
+import android.app.UiModeManager.MODE_NIGHT_YES
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
 import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,12 +24,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvUpdates: TextView
     private lateinit var tvAddress: TextView
 
+    // Switches
     lateinit var swLocationUpdates: SwitchCompat
     lateinit var swGps: SwitchCompat
+
+    // API for location services
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+
+
+    val fastUpdateInterval: Long = 5
+    val defaultUpdateInterval: Long = 30
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         tvLatitude = findViewById(R.id.tvLatitude)
         tvLongitude = findViewById(R.id.tvLongitude)
@@ -35,6 +51,23 @@ class MainActivity : AppCompatActivity() {
         tvAddress = findViewById(R.id.tvAddress)
         swLocationUpdates = findViewById(R.id.swLocationsUpdates)
         swGps = findViewById(R.id.swGps)
+
+        locationRequest = LocationRequest()
+
+        locationRequest.interval = 1000 * defaultUpdateInterval
+        locationRequest.fastestInterval = 1000 * fastUpdateInterval
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        swGps.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(swGps.isChecked){
+                locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                tvSensor.text = "Using GPS sensors"
+            }
+            else{
+                locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+                tvSensor.text = "Using Towers + Wifi"
+            }
+        }
 
 
     }
