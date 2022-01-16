736c808a197b728ccdffbaffb418aafe37a5e3ec
pwhited-7 <pacen-w77@hotmail.com>
2022-01-16 13:48:57 -0700

    Added maps camera position

--- app/src/main/java/com/example/location_app/MapsActivity.kt
+++ app/src/main/java/com/example/location_app/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.location_app
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,14 +14,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.location_app.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    lateinit var savedLocations: List<Location>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF6200EE")))
+        supportActionBar?.title = "Maps"
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,6 +35,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        var myApplication = applicationContext as MyApplication
+        savedLocations = myApplication.getMyLocation()
     }
 
     /**
@@ -42,7 +54,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        savedLocations.forEach { location: Location ->
+            var latLng = LatLng(location.latitude, location.longitude)
+
+            val perth = mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title("Lat: "+location.latitude+" Lon: "+ location.longitude)
+
+            )
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+        }
     }
 }
--- app/src/main/java/com/example/location_app/ShowSavedLocations.kt
+++ app/src/main/java/com/example/location_app/ShowSavedLocations.kt
@@ -1,10 +1,15 @@
 package com.example.location_app
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import java.lang.Exception
 
 class ShowSavedLocations : AppCompatActivity() {
 
@@ -13,13 +18,36 @@ class ShowSavedLocations : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_saved_locations)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF6200EE")))
+        supportActionBar?.title = "Waypoint List"
 
         lvSavedLocations = findViewById(R.id.lv_wayPoints)
 
         var myApplication: MyApplication = applicationContext as MyApplication
         var savedLocations: List<Location> = myApplication.getMyLocation()
+        var filteredLocations: MutableList<String> = mutableListOf<String>()
 
-        lvSavedLocations.adapter = ArrayAdapter<Location>(this, android.R.layout.simple_list_item_1, savedLocations)
+
+
+        savedLocations.forEach{ location: Location ->
+
+            val geocoder = Geocoder(this)
+
+            val address: String = try {
+                val addresses: List<Address> = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                addresses[0].getAddressLine(0)
+            }catch (e: Exception){
+                "Unable to get street address"
+            }
+
+            val str: String = "Latitude: " + location.latitude+ "     Longitude: " + location.longitude +"\n" +
+                    "Address: " + address
+
+            filteredLocations.add(str)
+        }
+
+
+        lvSavedLocations.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, filteredLocations)
     }
 
 }
